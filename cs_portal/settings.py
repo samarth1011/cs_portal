67f2fb2628dc13f9207c2a17f2cddae2df852f99
Samarth Godase <samarth.godase@yardi.com>
2025-04-20 18:38:04 +0530

    weffe

--- cs_portal/settings.py
+++ cs_portal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,6 +134,8 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # CKEditor Uploads
 CKEDITOR_UPLOAD_PATH = "uploads/"
